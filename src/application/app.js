e5d195b150b3e83823c54af54396ed508a4ae3d5
Mhd Aris <mhdarisid@gmail.com>
2024-06-03 10:57:23 +0700

    test deploy

--- src/application/app.js
+++ src/application/app.js
@@ -1,14 +1,12 @@
 import express from "express";
-import { router } from '../routes/user_route.js';
+import { router } from "../routes/user_route.js";
 import { exceptionMiddleware } from "../middlewares/exception_middleware.js";
-//import { publicRouter } from "../routes/test_route.js";
+import { publicRouter } from "../routes/test_route.js";
 
 export const app = express();
 app.use(express.json());
 
-
-//Router
-//app.use(publicRouter);
-app.use('/users', router);
+app.use(publicRouter);
+app.use("/users", router);
 
 app.use(exceptionMiddleware);
