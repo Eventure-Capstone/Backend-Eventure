7d5964dfb99f9938d2f5bd5d01b1ded4db780371
Zidan Dhiyaul Haq <93887282+zeedh@users.noreply.github.com>
2024-06-15 07:22:03 +0700

    Update user_route.js

--- src/routes/user_route.js
+++ src/routes/user_route.js
@@ -6,5 +6,11 @@ const router = express.Router();
 router.post("/register", userController.register);
 router.post("/verify", userController.verify);
 router.post("/login", userController.login);
+router.get("/users", userController.getAllUsers);
+router.get("/users/:id", userController.getUserById);
+router.delete("/users/:id", userController.deleteUser);
+router.put("/users/:id", userController.updateUser);
+router.post("/createEvent", userController.createEvent);
+
 
 export default router;
